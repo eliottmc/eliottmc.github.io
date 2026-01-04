98e4ba13bc1a357c64c283d1179b4363e590a42e
Mace Eliott <eliott.mace@student-cs.fr>
2026-01-04 16:25:31 +0100

    footer and legal mentions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {Row, Col} from 'react-bootstrap';
+
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer style={{textAlign: 'center', padding: '1rem', backgroundColor: '#6F8F7E', color: '#ffffff'}}>
-            <p>&copy; {new Date().getFullYear()} Mon Site de Dressage Canin. Tous droits réservés.</p>
+        <footer style={{textAlign: 'center', padding: '1rem', backgroundColor: '#6F8F7E', color: '#F5F3EE', fontSize: '0.9rem', lineHeight: '0.8rem'}}>
+            <Row style={{alignItems: 'center'}}>
+                <Col md={6}>
+                    <p>J{"'"}interviens à Paris et en proche banlieue.</p>
+                    <p>Mail : <a href="mailto:contact@davidmace.fr" style={{"color":"#F5F3EE"}}>contact@davidmace.fr</a></p>
+                    <p>Téléphone : 06 84 24 28 29</p>
+                </Col>
+                <Col md={6}>
+                    <p>&copy; {new Date().getFullYear()} David Macé - Tous droits réservés.</p>
+                    <p><a href="/legals" style={{"color":"#F5F3EE"}}>Mentions légales</a></p>
+                    <p>Icon made by yut1655 from <a href="https://www.flaticon.com/" style={{"color":"#F5F3EE"}}>www.flaticon.com</a></p>
+                    <p>Photos réalisées par Sylvie Biscioni</p>
+                </Col>
+            </Row>
         </footer>
     );
 }
---
+++ src/pages/Legals.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import {Container, Row} from 'react-bootstrap';
+
+import icon from '../assets/niche.png';
+
+const Legals = () => {
+  return (
+    <>
+      <head>
+        <title>David Macé - Éducateur comportementaliste canin</title>
+        <link rel="icon" href={icon}/>
+      </head>
+      <div className={"banner"}>
+          <div>
+            <Container data-scroll-index='1' style={{textAlign:'center'}}>
+              <h1>Mentions légales</h1>
+            </Container>
+          </div>
+      </div>
+      <Container data-scroll-index='2' style={{paddingBottom:'4rem', fontSize:'0.9rem', lineHeight: '1rem'}}>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+          <p>Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. 
+            En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités. 
+            Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, 
+            les responsables du présent site internet <a href="https://davidmace.fr/">https://davidmace.fr/</a> sont :</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Éditeur du site</h5>
+            <p>David Macé</p>
+            <p>122 avenue Parmentier, 75011 Paris</p>
+            <p>Numéro de téléphone : 06 84 24 28 29</p>
+            <p>Adresse email : contact@davidmace.fr</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Hébergement</h5>
+            <p>Hébergeur : Microsoft Azure</p>
+            <p>37, Quai du Président Roosevelt, 92130 Issy-les-Moulineaux</p>
+            <p>Numéro de téléphone : 09 70 01 90 90</p>
+            <p>Site : <a href="https://azure.microsoft.com/">https://azure.microsoft.com/</a></p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Développement</h5>
+            <p>Eliott Macé</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Cookies</h5>
+            <p>Le site <a href="https://davidmace.fr/">https://davidmace.fr/</a> peut-être amené à vous demander l’acceptation des cookies 
+            pour des besoins de statistiques et d’affichage. Un cookies est une information déposée sur votre disque dur par 
+            le serveur du site que vous visitez. Il contient plusieurs données qui sont stockées sur votre ordinateur dans un simple 
+            fichier texte auquel un serveur accède pour lire et enregistrer des informations. Certaines parties de ce site ne peuvent être 
+            fonctionnelles sans l’acceptation de cookies.</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Services fournis</h5>
+            <p>L’ensemble des activités de la société ainsi que ses informations sont présentés sur notre site <a href="https://davidmace.fr/">https://davidmace.fr/</a>.
+            David Macé s’efforce de fournir sur le site <a href="https://davidmace.fr/">https://davidmace.fr/</a> des informations aussi précises que possible. 
+            les renseignements figurant sur le site <a href="https://davidmace.fr/">https://davidmace.fr/</a> ne sont pas exhaustifs et les photos non contractuelles. 
+            Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. 
+            Par ailleurs, tous les informations indiquées sur le site <a href="https://davidmace.fr/">https://davidmace.fr/</a> sont données à titre indicatif, 
+            et sont susceptibles de changer ou d’évoluer sans préavis.</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Limitation contractuelles sur les données</h5>
+            <p>Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, 
+                mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, 
+                merci de bien vouloir le signaler par email, à l’adresse <a href="mailto:contact@davidmace.fr">contact@davidmace.fr</a>, 
+                en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …). 
+                Tout contenu téléchargé se fait aux risques et périls de l’utilisateur et sous sa seule responsabilité. En conséquence, 
+                ne saurait être tenu responsable d’un quelconque dommage subi par l’ordinateur de l’utilisateur ou d’une quelconque perte de données consécutives 
+                au téléchargement. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et 
+                avec un navigateur de dernière génération mis-à-jour. Les liens hypertextes mis en place dans le cadre du présent site internet en direction 
+                d’autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de David Macé.</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Propriété intellectuelle</h5>
+            <p>Tout le contenu du présent sur le site <a href="https://davidmace.fr/">https://davidmace.fr/</a>, incluant, de façon non limitative, 
+            les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive 
+            de la société à l’exception des marques, logos ou contenus appartenant à d’autres sociétés partenaires ou auteurs. 
+            Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments 
+            est strictement interdite sans l’accord exprès par écrit de David Macé. Cette représentation ou reproduction, par quelque procédé que ce soit, 
+            constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. 
+            Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. 
+            En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.</p>
+        </Row>
+        <Row className="text-row" style={{marginBottom:'2rem'}}>
+            <h5>Données personnelles</h5>
+            <p>De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a href="https://davidmace.fr/">https://davidmace.fr/</a>.
+             Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, 
+             vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre fonction, le nom de votre société, votre adresse électronique, 
+             et votre numéro de téléphone. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « contact ». 
+             Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site, 
+             notamment celui de solliciter des renseignements sur notre société, ou de recevoir les lettres d’information. 
+             Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, 
+             notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, 
+             le type de votre navigateur, vos temps d’accès. De telles informations sont utilisées exclusivement à des fins de statistiques internes, 
+             de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 
+             transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Legals;
