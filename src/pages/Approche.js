eb28106b419030c2179b2293936a1570f1522708
Mace Eliott <eliott.mace@student-cs.fr>
2026-01-14 17:45:59 +0100

    filled in "approche" page

--- src/pages/Approche.js
+++ src/pages/Approche.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Container, Col, Row} from 'react-bootstrap';
 
-import '../styles/HomePage.css';
+import '../styles/Approche.css';
 
-import david_gaston_bridge from '../assets/pictures_gaston_david/chien_maitre_ville_paris.JPG';
-import gaston_head from '../assets/pictures_gaston_david/chien-ville-paris-vertical.JPG';
 import icon from '../assets/niche.png';
 import david_gaston_reading from '../assets/pictures_gaston_david/chien_maitre_ville_paris_education_positive.JPG';
+
 
 const Approche = () => {
   return (
@@ -20,62 +19,47 @@ const Approche = () => {
       <div className={"banner"}>
           <div>
             <Container data-scroll-index='1' style={{textAlign:'center'}}>
-              <h1>David Macé</h1>
-              <h3 style={{marginBottom: '0px'}}>Éducateur canin comportementaliste</h3>
-              <h3 style={{paddingTop: '0px'}}>Paris et proche banlieue</h3>
-              <div className="img-cropper">
-                <img src={gaston_head} alt="chien-ville-paris" className="banner-img" />
-              </div>
-              <p>Titulaire ACACED</p>
-              <p>Formation d{"'"}éducation, de rééducation comportementale et d{"'"}intégration sociale canines</p>
-              <p>Certifié RSCH n°6621</p>
+              <h1>Mon approche</h1>
             </Container>
           </div>
       </div>
       <Container data-scroll-index='2' style={{marginTop:'6rem', textAlign:'justify', paddingBottom:'4rem', paddingLeft:'10rem', paddingRight:'10rem'}}>
-        <Row className="align-items-center" style={{marginBottom:'4rem'}}>
-          <Col md={6}>
-            <p>Éducateur canin spécialisé dans les problématiques éducatives et comportementales,
-            j’interviens principalement à <b>Paris et en proche banlieue</b>.</p>
-            <p>Je vous accompagne de manière individualisée, pour un bilan comportemental, des séances
-            d’éducation ou de suivi, une préparation à l’adoption. Ponctuellement ou régulièrement.
-            Suivant vos disponibilités et vos besoins. Avec comme seul objectif de vous aider à mieux
-            comprendre votre chien et construire une relation sereine.</p>
-            <p>Formé à l’<b>éducation positive</b> au centre Entre Chiens, auprès de Gaston Mellet, Yann Forêt et
-            du docteur vétérinaire Sylvia Masson, je propose une approche non coercitive, respectueuse,
-            et adaptée au quotidien des chiens vivant en milieu urbain.</p>
+        <Row className="align-items-stretch" style={{marginBottom:'2rem'}}>
+        <h3> <span className='highlight'>Une approche individualisée et adaptée à la vie urbaine</span></h3>
+        <h3>Chaque chien, comme chaque humain, est unique.</h3>
+          <Col md={4} style={{display: 'flex'}}>
+            <img src={david_gaston_reading} alt="chien-maitre-ville-paris-education-positive" style={{width: '100%', objectFit: 'cover'}} className='presentation-img'/>
           </Col>
-          <Col md={6}>
-            <img src={david_gaston_bridge} alt="chien-maitre-ville-paris" className="presentation-img" />
+          <Col md={8} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+            <p>Il n’existe pas de séance « type », ni de frontière rigide entre éducation et comportement.
+                Mon travail est toujours un accompagnement <b>adapté à votre chien, à votre environnement
+                et à votre quotidien</b>, à Paris comme en proche banlieue, <b>ainsi qu’à la problématique que
+                vous m’apportez</b>.</p>
+            <p>Un chien qui tire en laisse : </p>
+            <ul>
+              <li>Est-ce parce qu’il n’a pas appris à marcher en laisse ?</li>
+              <li>Est-ce parce qu’il a beaucoup d’énergie et pas assez d’occasions de la dépenser ?</li>
+              <li>Est-ce parce qu’il réagit à certains stimuli (chiens, humains, bruits, véhicules) ?</li>
+            </ul>
+            <p style={{marginBottom:'0'}}>Dans le premier cas, il s{"'"}agit d{"'"}éducation : <b>une séance peut suffire</b> pour vous montrer comment lui apprendre.</p>
+            <p style={{marginBottom:'0'}}>Dans le second cas, il sera nécessaire de faire un point sur ses <b>besoins fondamentaux</b>.</p>
+            <p>Dans le dernier cas, un <b>travail de désensibilisation</b> est indispensable.</p>
+            <p style={{marginBottom:'0'}}><b>Tous les chiens n{"'"}apprennent pas au même rythme.</b></p>
+            <p style={{marginBottom:'0'}}>Certains comprennent très vite, d’autres ont besoin de plus de temps.</p>
+            <p style={{marginBottom:'0'}}>C’est une question d’individualité, d’histoire et de contexte.</p>
           </Col>
         </Row>
         <Row className="align-items-center">
-          <Col md={4}>
-            <img src={david_gaston_reading} alt="chien-maitre-ville-paris-education-positive" style={{maxHeight: '600px'}} className='presentation-img'/>
-          </Col>
-          <Col md={8}>
-            <p>Mon travail consiste à vous proposer des solutions concrètes aux problèmes que vous
-              rencontrez, adaptées à votre chien, et à vous accompagner dans son éducation en tenant
-              compte de votre environnement et de votre mode de vie.</p>
-            <p>Ce travail se fait avec votre participation. En vous impliquant, vous offrez à votre chien des
-              moments de complicité et de plaisir partagés, tout en renforçant votre lien.</p>
-            <p>Vous verrez qu’avec un peu de travail et de patience, un chien est capable de tout apprendre,
-              tout comprendre : marche en laisse, rappel, statique, accueil des visiteurs, règles du
-              quotidien… La stimulation intellectuelle fait partie de son équilibre et de ses besoins
-              fondamentaux, et ceci à tout âge.</p>
-            <p>Et s’il aboie, tire sur sa laisse, détruit le canapé, qu’il a du mal à gérer ses émotions, ce n’est
-              certainement pas pour vous fâcher, c’est souvent juste un problème de communication. Et ça
-              aussi ça s’apprend et se travaille. Tout ce qui l’intéresse (à part peut-être manger), c’est de
-              vous rendre heureux. C’est vrai pour tous les chiens.</p>
-          </Col>
+          <p>Vous souhaitez <b>apprendre à votre chiot</b> à s’asseoir, à attendre, à ne pas bouger, à lâcher un objet, ou même à réaliser quelques tours amusants ?</p>
+          <p>En <b>une séance d’une heure</b>, vous pouvez acquérir les bases nécessaires pour travailler un ou deux apprentissages techniques.</p>
+          <p style={{marginBottom:'0'}}>{'"'}<b>Mon chien ne m{"'"}obéit pas</b>{'"'}</p>
+          <p>C’est une phrase qu’on entend souvent.</p>
+          <p style={{marginBottom:'0'}}>En réalité, la première question à se poser serait plutôt :</p>
+          <p><b>Est-ce que mon chien comprend ce que je lui demande ?</b></p>
+          <p>Très souvent, ce qui est perçu comme un problème d’éducation est en réalité <b>un problème de communication</b>.</p>
         </Row>
         
         <Row className="align-items-center text-center g-0" style={{marginTop:'4rem', justifyContent:'center'}}>  
-          <Col style={{maxWidth:'400px'}}>
-            <button className="button" onClick={() => window.location.href='/prestations'}>
-              Découvrir mes prestations
-            </button>
-          </Col>
           <Col style={{maxWidth:'400px'}}>
             <button className="button" onClick={() => window.location.href='/contact'}>
               Me contacter
