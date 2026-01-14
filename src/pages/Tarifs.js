c1c2cff948004a30438384f197d036091cad2103
Mace Eliott <eliott.mace@student-cs.fr>
2026-01-14 19:11:03 +0100

    fix tarifs page

--- src/pages/Tarifs.js
+++ src/pages/Tarifs.js
@@ -22,13 +22,13 @@ const Tarifs = () => {
           </Container>
         </div>
       </div>
-      <Container data-scroll-index='2' style={{marginTop:'6rem', paddingBottom:'4rem', paddingLeft:'10rem', paddingRight:'5rem'}} className="presta-container">
+      <Container data-scroll-index='2' style={{marginTop:'6rem', paddingBottom:'4rem', paddingLeft:'10rem', paddingRight:'10rem'}} className="presta-container">
         <div style={{marginBottom:'3rem', textAlign:'justify'}}>
               <p style={{marginBottom:'0'}}>Je vous propose un échange téléphonique gratuit d’environ <b>dix minutes</b>, durant lequel vous
                   pourrez m’exposer votre situation, vos attentes et vos difficultés.</p>
               <p style={{marginBottom:'0'}}>Cet échange permet de faire un premier point sur votre situation et de voir comment je peux vous accompagner.</p>
         </div>
-          <Row className="align-items-stretch">
+          <Row className="">
             <Col md={8}>
             <div style={{marginBottom:'3rem'}}>
               <h4><span className="highlight">Séances</span></h4>
@@ -41,20 +41,6 @@ const Tarifs = () => {
               <p>Les frais de déplacement ainsi qu’un <p>compte rendu écrit de séance</p> sont inclus dans ces tarifs.</p>
             </div>
             <div style={{marginBottom:'3rem'}}>
-              <h4><span className="highlight">Adoption en refuge/association</span></h4>
-              <p>Une remise de <b>20 %</b> est proposée :</p>
-              <ul>
-                <li>sur le <b>bilan</b></li>
-                <li>ou sur la <b>première séance</b> (éducative, comportementale ou préparation à l’adoption)</li>
-              </ul>
-              <p>pour les chiens adoptés en refuge ou en association, sur présentation d’un justificatif.</p>
-            </div>
-            </Col>
-            <Col style={{display:'flex'}} md={4}>
-              <img src={maliboo} alt="chien-heureux-education-positive" className="tarif_img" />
-            </Col>
-            </Row>
-            <div style={{marginBottom:'3rem'}}>
               <h4><span className="highlight">Conditions particulières</span></h4>
               <p>Une majoration de <b>25 %</b> est appliquée pour les rendez-vous :</p>
               <ul>
@@ -63,6 +49,20 @@ const Tarifs = () => {
                 <li>et les jours fériés</li>
               </ul>
               <p><b>Les séances en visioconférence et les créneaux en horaires décalés font l’objet d’un paiement à l’avance.</b></p>
+            </div>
+            </Col>
+            <Col md={4}>
+              <img src={maliboo} alt="chien-heureux-education-positive" className="tarif_img" />
+            </Col>
+            </Row>
+            <div style={{marginBottom:'3rem'}}>
+              <h4><span className="highlight">Adoption en refuge/association</span></h4>
+              <p>Une remise de <b>20 %</b> est proposée :</p>
+              <ul>
+                <li>sur le <b>bilan</b></li>
+                <li>ou sur la <b>première séance</b> (éducative, comportementale ou préparation à l’adoption)</li>
+              </ul>
+              <p>pour les chiens adoptés en refuge ou en association, sur présentation d’un justificatif.</p>
             </div>
             <Row className="align-items-center text-center" style={{marginTop:'4rem'}}>
               <div style={{maxWidth:'400px', margin:'0 auto'}}> 
