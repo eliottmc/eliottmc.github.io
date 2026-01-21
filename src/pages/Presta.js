64596d19e8fc9ee560f9eef31257f7e6734eb543
Mace Eliott <eliott.mace@student-cs.fr>
2026-01-21 19:32:40 +0100

    modifs presta

--- src/pages/Presta.js
+++ src/pages/Presta.js
@@ -25,18 +25,16 @@ const Presta = () => {
         <div className="presta-container">
           <div >
             <div style={{marginBottom:'3rem', textAlign:'justify'}}>
-              <p style={{marginBottom:'0'}}>Chaque séance dure environ une heure.</p>
-              <p style={{marginBottom:'0'}}>Au-delà, les chiens perdent en concentration et en disponibilité.</p>
-              <p>Ce n’est pas une séance de dressage mais un moment de plaisir, d’apprentissage et de complicité entre lui et son humain.</p>
-              <p>Mon but en temps qu’éducateur canin étant de consolider le lien entre le chien et son maître, votre présence est souhaitable. </p>
+              <p>Chaque séance dure environ <b>une heure</b>.</p>
+              <p>Mon but en temps qu’éducateur canin étant de <b>consolider le lien</b> entre le chien et son maître, votre présence est souhaitable. </p>
               <p>Les séances ont lieu <b>à domicile ou en extérieur</b>, selon l’apprentissage à mettre en place.</p>
               <p>Je me déplace à Paris ainsi qu’en proche banlieue.</p>
-              <p>Des séances en visioconférence sont possibles, lorsque la problématique le permet.</p>
-              <p>Un compte rendu écrit, reprenant les points travaillés et les axes à poursuivre, vous sera transmis dans les jours suivant la séance.</p>
+              <p>Des <b>séances en visioconférence</b> sont possibles, lorsque la problématique le permet.</p>
+              <p>Un <b>compte rendu écrit</b>, reprenant les points travaillés et les axes à poursuivre, vous sera transmis dans les jours suivant la séance.</p>
               <p>Je reste ensuite joignable pour toute précision ou question complémentaire.</p>
             </div>
             <div style={{marginBottom:'3rem'}}>
-              <h3><span className="highlight">Séance éducative</span></h3>
+              <h3>Séance éducative</h3>
               <p>
                 Les séances éducatives permettent de travailler un ou deux apprentissages précis, en fonction de vos besoins et de ceux de votre chien.</p>
               <p>
@@ -47,7 +45,7 @@ const Presta = () => {
               <p>À l’issue de la séance, vous disposez des <b>outils nécessaires pour poursuivre l’apprentissage en autonomie</b>.</p>
             </div>
             <div style={{marginBottom:'3rem'}}>
-              <h3><span className="highlight">Séance comportementale</span></h3>
+              <h3>Séance comportementale</h3>
               <p>
                 Les séances comportementales s’adressent aux problématiques telles que les aboiements
                 excessifs, les peurs ou phobies, les difficultés de gestion émotionnelle, tout comportement
@@ -56,21 +54,21 @@ const Presta = () => {
                 votre environnement aussi variés que l’arrivée d’un bébé, une séparation, un déménagement,
                 ou toute autre modification susceptible de perturber l’équilibre de votre chien.</p>
               <p>La première séance permet de comprendre la situation, d’identifier les déclencheurs et le contexte, puis de définir une stratégie de travail adaptée.</p>
-              <p>Chaque situation est évaluée individuellement : certains problèmes peuvent se résoudre rapidement,
+              <p><b>Chaque situation est évaluée individuellement</b> : certains problèmes peuvent se résoudre rapidement,
                 parfois en une séance, d’autres nécessitent un accompagnement progressif.</p>
             </div>
             <div style={{marginBottom:'3rem'}}>
-              <h3><span className="highlight">Préparation à l{"'"}adoption</span></h3>
+              <h3>Préparation à l{"'"}adoption</h3>
               <p>
-                La préparation à l’adoption permet d’anticiper l’arrivée d’un chien et de construire un projet
+                La préparation à l’adoption permet d’<b>anticiper l’arrivée d’un chien</b> et de construire un projet
                 cohérent, avant et/ou après son arrivée au sein du foyer.</p>
-              <p>Selon vos envies et vos besoins, nous aborderons le choix du chien, votre environnement et
+              <p>Selon <b>vos envies et vos besoins</b>, nous aborderons le choix du chien, votre environnement et
                 mode de vie, les besoins fondamentaux du chien, les habitudes à mettre en place, ainsi que
                 toutes les problématiques liées à l’arrivée d’un chiot à Paris.</p>
               <p>Un accompagnement plus poussé peut être proposé <b>sur devis</b>.</p>
             </div>
             <div style={{marginBottom:'3rem'}}>
-              <h3><span className="highlight">Séance en visioconférence</span></h3>
+              <h3>Séance en visioconférence</h3>
               <p>
                 Les séances en visioconférence permettent un travail ciblé, notamment pour le suivi éducatif
                 ou comportemental, certaines problématiques spécifiques et les situations ne nécessitant pas
